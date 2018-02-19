a8fcd4a83d7e015f7c186a458e964704c26c210d
Andrei Olaru <andrei.olaru@starbyte.ro>
2018-02-19 19:07:18 +0200

    removed old app folder, did not work with hmr

--- Tasks.Web/ClientApp/app/components/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { MatButton } from '@angular/material';
-
-@Component({
-    selector: 'home',
-    templateUrl: './home.component.html'
-})
-export class HomeComponent {
-}
